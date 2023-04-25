64789bf5428745640f9b5ad5acdc462a458a61c5
UC hayato <c0a221104a@edu.teu.ac.jp>
2023-04-25 15:44:12 +0900

    練習５

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,12 +4,29 @@ import sys
 import pygame as pg
 
 delta = {
-    pg.K_UP: (0, -1),
-    pg.K_DOWN: (0, +1),
-    pg.K_LEFT: (-1, 0),
-    pg.K_RIGHT: (+1, 0)
+    pg.K_UP: (0, -1),  #  上矢印キーが押されたときの移動
+    pg.K_DOWN: (0, +1),  #  下矢印キーが押されたときの移動
+    pg.K_LEFT: (-1, 0),  #  左矢印キーが押されたときの移動
+    pg.K_RIGHT: (+1, 0)  #  右矢印キーが押されたときの移動
 
 }
+
+
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
+    引数１：画面surfaceのrect
+    引数２：こうかとん、または、爆弾surfaceのrect
+    戻り値：横方向、縦方向のはみ出し判定結果(画面内：True、画面外：False)
+    """
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+
+    return yoko, tate 
+    
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -27,7 +44,7 @@ def main():
     bb_rect = bb_img.get_rect()  #rectクラスを定義
     bb_rect.center = x, y  # 中心位置をランダムに設定
     kk_rect = kk_img.get_rect()
-    kk_rect.center = 900, 400
+    kk_rect.center = 900, 400  #初期座標の設定
     tmr = 0
 
     while True:
@@ -37,13 +54,22 @@ def main():
 
         tmr += 1
 
-        key_lst = pg.key.get_pressed()
-        for k, mv in delta.items():
-            if key_lst[k]:
+        key_lst = pg.key.get_pressed()  #  キーの押下状態のリストを修得
+        for k, mv in delta.items():  #  辞書の値を代入
+            if key_lst[k]:  #  指定されているキーが押されたとき
                 kk_rect.move_ip(mv)
+        if check_bound(screen.get_rect(), kk_rect) != (True, True):
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rect.move_ip(-mv[0], -mv[1])
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rect)
         bb_rect.move_ip(vx, vy)
+        yoko, tate = check_bound(screen.get_rect(), bb_rect)
+        if not yoko:
+            vx*= -1
+        if not tate:
+            vy*= -1
         screen.blit(bb_img, bb_rect)
 
 
